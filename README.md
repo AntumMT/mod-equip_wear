0ae564ce8602278f5af328d45593097ab34a11f9
Jordan Irwin <antumdeluge@gmail.com>
2021-07-14 19:38:05 -0700

    Add disable weapon wear

--- README.md
+++ README.md
@@ -17,9 +17,15 @@ tool_wear
 - Enables/Disables wear for tools such as picks, axes, shovels, etc.
 - type:    bool
 - default: true
+
+weapon_wear
+- Enables/Disables wear for weapons.
+- type:    bool
+- default: true
 ```
 
 ### Links:
 
+- [Git repo](https://github.com/AntumMT/mod-equip_wear)
 - [Changelog](changelog.txt)
 - [TODO](TODO.txt)
--- init.lua
+++ init.lua
@@ -1,14 +1,34 @@
 
 local tool_wear = core.settings:get_bool("tool_wear", true)
+local weapon_wear = core.settings:get_bool("weapon_wear", true)
 
 core.register_on_mods_loaded(function()
+	-- hook into ItemStack meta table method
+	if not weapon_wear then
+		local mt = getmetatable(ItemStack(""))
+		local add_wear_old = mt.add_wear
+		mt.add_wear = function(self, ...)
+			local i_type = core.registered_items[self:get_name()].type
+			if i_type and i_type == "tool" then
+				return true
+			end
+
+			return add_wear_old(self, ...)
+		end
+	end
+
 	for tname, tdef in pairs(core.registered_tools) do
 		local new_def = table.copy(tdef)
 		local override = false
 
 		if new_def.tool_capabilities then
+			if not weapon_wear then
+				new_def.tool_capabilities.punch_attack_uses = nil
+			end
+
 			if new_def.tool_capabilities.groupcaps then
 				for k, v in pairs(new_def.tool_capabilities.groupcaps) do
+					-- FIXME: should "fleshy" only be modified for weapon wear?
 					if not tool_wear then
 						new_def.tool_capabilities.groupcaps[k].uses = 0
 						override = true
